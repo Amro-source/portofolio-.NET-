e9e29f60e74ca457d00583d704b69321687dfd17
Amro <68817735+Amro-source@users.noreply.github.com>
2025-06-08 18:07:32 +0300

    Add files via upload

---
+++ Number Guessing Game/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class GuessingGame
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int secretNumber = rand.Next(1, 101);
+        int guess = 0;
+        int attempts = 0;
+
+        Console.WriteLine("Guess the number between 1 and 100!");
+
+        while (guess != secretNumber)
+        {
+            Console.Write("Enter your guess: ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if (guess < secretNumber)
+                Console.WriteLine("Too low!");
+            else if (guess > secretNumber)
+                Console.WriteLine("Too high!");
+        }
+
+        Console.WriteLine($"Correct! You guessed it in {attempts} attempts.");
+    }
+}
